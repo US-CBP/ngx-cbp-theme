961e11e74ef13df5c48c4eca1fc9bd47815c7c43
Yogesh <themodernizers@gmail.com>
2017-09-08 00:43:19 -0400

    using ngx-library-builder as npm library

--- tools/ngx-lib-build.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const builder = require('./builder');
-const path = require('path');
-
-builder.build({rootFolder: path.resolve('.'), srcFolder : path.resolve('src', 'app')});
-
-
-
