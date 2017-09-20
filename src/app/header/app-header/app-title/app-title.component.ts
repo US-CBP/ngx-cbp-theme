328b8d0e098a9d27b8cdf26c134e15fea3203910
yogeshgadge <yogeshgadge@gmail.com>
2017-09-20 14:16:19 -0400

    mobile responsive app-header demo

--- src/app/header/app-header/app-title/app-title.component.ts
+++ src/app/header/app-header/app-title/app-title.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'cbp-app-title',
   templateUrl: './app-title.component.html',
-  styleUrls: ['./app-title.component.scss']
+  styleUrls: ['./app-title.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CBPAppTitleComponent implements OnInit {
 
