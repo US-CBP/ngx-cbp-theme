54b70092c03c12f5d4d61f5fe773d0e2f3870b08
yogeshgadge <yogeshgadge@gmail.com>
2017-09-22 09:46:12 -0400

    unit testing demo covers unit tests of "lib" since "lib" does not bootstrap

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,27 +1,4 @@
-// import {NgModule} from '@angular/core';
-//
-// import {CBPHeaderModule} from './header';
-// import {CBPAccordionModule} from './accordion/index';
-// import {CBPRootModule} from './cbp-root';
-// import {CBPAppHeaderModule} from './header';
-// import {CBPProgressModule} from './progress/progress.module';
-//
-// const ALL_MODULES = [CBPHeaderModule, CBPAccordionModule, CBPRootModule, CBPAppHeaderModule, CBPProgressModule];
-//
-// /**
-//  * Use individual modules instead of NgxCBPModule. This module strictly serves as entry module for AOT & testing.
-//  */
-// @NgModule({
-//     imports: ALL_MODULES,
-//     exports: ALL_MODULES
-// })
-// export class NgxCBPModule {}
-
-
-
 export * from './accordion/index';
-
-
 export * from './applications/index';
 export * from './cbp-root/index';
 export * from './header/index';
