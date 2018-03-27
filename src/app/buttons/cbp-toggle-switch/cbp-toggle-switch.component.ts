743b0cf574cb3d8e49ebabccbb8c877e7f993d1b
yogeshgadge <yogeshgadge@gmail.com>
2018-03-27 17:18:49 -0400

    cbp-toggle-button change detection OnPush

--- src/app/buttons/cbp-toggle-switch/cbp-toggle-switch.component.ts
+++ src/app/buttons/cbp-toggle-switch/cbp-toggle-switch.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 let toggleSwitchCounter = 1;
 
@@ -6,32 +6,31 @@ let toggleSwitchCounter = 1;
 @Component({
   selector: 'cbp-toggle-switch',
   templateUrl: './cbp-toggle-switch.component.html',
-  styleUrls: ['./cbp-toggle-switch.component.scss']
+  styleUrls: ['./cbp-toggle-switch.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CBPToggleSwitchComponent implements OnInit {
+export class CBPToggleSwitchComponent {
 
   @Input() onLabel = 'ON';
   @Input() offLabel = 'OFF';
   @Input() onValue = true;
   @Input() offValue = false;
   @Input() label: string = null;
-  @Input() initialValue: any;
   @Input() required: boolean;
   @Input() disabled: boolean;
   @Output() changed = new EventEmitter<boolean>();
 
-  isOn: Boolean;
+  @Input() isOn: Boolean;
 
   toggleSwitchId = `cbp-toggle-sw-${toggleSwitchCounter}`;
 
   constructor() {
       toggleSwitchCounter++;
   }
-  ngOnInit() {
-      this.isOn = this.initialValue === this.onValue;
-  }
+
 
   valueChange($event: any) {
-    this.changed.emit($event  ? this.onValue : this.offValue);
+    this.isOn = $event.currentTarget.checked  ? this.onValue : this.offValue;
+    this.changed.emit(this.isOn.valueOf());
   }
 }
--- src/kitchensink/app/demo-buttons/demo-buttons.module.ts
+++ src/kitchensink/app/demo-buttons/demo-buttons.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import {DemoButtonsComponent} from './demo-buttons.component';
 import {MatButtonModule} from '@angular/material';
 import {CBPButtonsModule} from '../../../app/buttons/buttons.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule, MatButtonModule, CBPButtonsModule
+    CommonModule, MatButtonModule, CBPButtonsModule, FormsModule
   ],
   exports: [DemoButtonsComponent, MatButtonModule],
   declarations: [DemoButtonsComponent]
