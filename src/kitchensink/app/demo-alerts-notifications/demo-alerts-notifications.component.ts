14368c3dedf28037ba3ff1922f7fba5d96fa7c42
yogeshgadge <yogeshgadge@gmail.com>
2018-03-29 22:08:04 -0400

    notifications & alerts demo cleanup

--- src/kitchensink/app/demo-alerts-notifications/demo-alerts-notifications.component.ts
+++ src/kitchensink/app/demo-alerts-notifications/demo-alerts-notifications.component.ts
@@ -1,5 +1,4 @@
-import {Component, Directive, OnInit} from '@angular/core';
-import {animate, style, transition, trigger} from '@angular/animations';
+import {Component,  OnInit} from '@angular/core';
 
 @Component({
   selector: 'demo-alerts-notifications, cbp-demo-alerts-notifications',
@@ -16,33 +15,5 @@ export class DemoAlertsNotificationsComponent implements OnInit {
 
   toggleToasts() {
       this.showing = ! this.showing;
-  }
-}
-
-@Component({
-    selector: 'demo-animated-toast, cbp-demo-animated-toast',
-    animations: [
-        trigger(
-            'enterAnimation', [
-                transition(':enter', [
-                    style({transform: 'translateX(100%)', opacity: 0}),
-                    animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
-                ]),
-                transition(':leave', [
-                    style({transform: 'translateX(0)', opacity: 1}),
-                    animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
-                ])
-            ]
-        )
-    ],
-    template: `<div *ngIf="show" [@enterAnimation]><ng-content></ng-content></div>`
-})
-export class DemoAnimateToastComponent {
-  private show = false;
-  showToast() {
-    this.show = true;
-  }
-  hideToast() {
-      this.show = false;
   }
 }
