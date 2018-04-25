64d92a3b5fdfc13ff9697725cbc6e13bf67453c3
yogeshgadge <yogeshgadge@gmail.com>
2018-04-25 13:34:59 -0400

    notification demo with markup

--- src/kitchensink/app/demo-notifications/demo-notifications.module.ts
+++ src/kitchensink/app/demo-notifications/demo-notifications.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoNotificationsComponent } from './demo-notifications.component';
 import {CBPNotificationsModule} from '../../../app/notifications';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule, MatButtonModule, CBPNotificationsModule
+    CommonModule, MatButtonModule, CBPNotificationsModule, MatIconModule
   ],
   declarations: [DemoNotificationsComponent],
   exports: [DemoNotificationsComponent]
