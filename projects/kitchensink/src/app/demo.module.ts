1364d13134cdc78600c9347814762f41260569e8
Yogesh <themodernizers@gmail.com>
2018-05-20 02:21:06 -0400

    fixing strict compilation errors of ng6

--- projects/kitchensink/src/app/demo.module.ts
+++ projects/kitchensink/src/app/demo.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 // import { CBPRootModule} from '../../app/cbp-root' ;
-import { CBPHeaderModule, CBPAppHeaderModule} from '../../app/header' ;
-import { CBPAccordionModule} from '../../app/accordion' ;
+import {} from '../../app/header' ;
+import {} from '../../app/accordion' ;
 
-import { DemoAppComponent } from './demo.component';
-import { MockUserService} from '../../mock-services/user.mock.service';
+import {DemoAppComponent} from './demo.component';
+import {MockUserService} from '../../../mock-services/user.mock.service';
 
 import {MatIconModule, MatTabsModule} from '@angular/material';
 
@@ -14,57 +14,62 @@ import {DemoTypographyComponent} from './demo-typography/demo-typography.compone
 import {DemoButtonsModule} from './demo-buttons/demo-buttons.module';
 import {DemoAppHeaderModule} from './demo-app-header/demo-app-header.module';
 import {HttpClientModule} from '@angular/common/http';
-import {MockApplicationsService} from '../../mock-services/applications.mock.service';
-import {CBP_USER_SERVICE} from '../../app/user/user';
-import {CBP_APPLICATIONS_SERVICE} from '../../app/applications/cbp-applications-service';
-import * as pkg from '../../../package.json';
+import {MockApplicationsService} from '../../../mock-services/applications.mock.service';
+import * as pkg from '../../../../package.json';
 import {DemoNotificationsModule} from './demo-notifications/demo-notifications.module';
-import {CBPNotificationsModule} from '../../app/notifications';
-import {CBPRootModule} from 'ngx-cbp-theme';
+import {
+  CBPRootModule,
+  CBPHeaderModule,
+  CBPAppHeaderModule,
+  CBPAccordionModule,
+  CBP_USER_SERVICE,
+  CBP_APPLICATIONS_SERVICE,
+  CBPNotificationsModule
+} from 'ngx-cbp-theme';
 
 export const KITCHENSINK_APP_VERSION = (<any>pkg).version;
 
 @NgModule({
   declarations: [
-      DemoAppComponent,
-      DemoCBPAccordionComponent,
-      DemoTypographyComponent
+    DemoAppComponent,
+    DemoCBPAccordionComponent,
+    DemoTypographyComponent
   ],
   imports: [
-      BrowserModule, HttpClientModule,
-      MatTabsModule, MatIconModule, // MdTabsModule is used only for demo cbp-app-header
-      CBPRootModule,
-      CBPAccordionModule,
-      CBPHeaderModule,
-      CBPAppHeaderModule,
-      CBPNotificationsModule,
-      // CBPProgressModule,
-      // demo
-      DemoButtonsModule,
-      DemoAppHeaderModule,
-      DemoNotificationsModule
+    BrowserModule, HttpClientModule,
+    MatTabsModule, MatIconModule, // MdTabsModule is used only for demo cbp-app-header
+    CBPRootModule,
+    CBPAccordionModule,
+    CBPHeaderModule,
+    CBPAppHeaderModule,
+    CBPNotificationsModule,
+    // CBPProgressModule,
+    // demo
+    DemoButtonsModule,
+    DemoAppHeaderModule,
+    DemoNotificationsModule
   ],
   exports: [DemoButtonsModule, DemoAppHeaderModule],
   providers: [
-      MockUserService,
-      MockApplicationsService,
-      { provide: CBP_USER_SERVICE,          useExisting: MockUserService },
-      { provide: CBP_APPLICATIONS_SERVICE,  useExisting: MockApplicationsService }
+    MockUserService,
+    MockApplicationsService,
+    {provide: CBP_USER_SERVICE, useExisting: MockUserService},
+    {provide: CBP_APPLICATIONS_SERVICE, useExisting: MockApplicationsService}
   ],
   schemas: [],
   bootstrap: [DemoAppComponent]
 })
 export class DemoAppModule {
-    constructor(applicationsService: MockApplicationsService, userService: MockUserService) {
-        // Handle your SSO login here for now
-        userService.loginInProgress = true;
-        // set this delay to zero if already loggedIn
-        // and implement userService so that getUser() immediately returns subject after login()
-        userService.login(3000);
-        applicationsService.getCurrentApp().subscribe(currentApp => {
-            currentApp.version =  `v${KITCHENSINK_APP_VERSION}`;
-        });
-    }
+  constructor(applicationsService: MockApplicationsService, userService: MockUserService) {
+    // Handle your SSO login here for now
+    userService.loginInProgress = true;
+    // set this delay to zero if already loggedIn
+    // and implement userService so that getUser() immediately returns subject after login()
+    userService.login(3000);
+    applicationsService.getCurrentApp().subscribe(currentApp => {
+      currentApp.version = `v${KITCHENSINK_APP_VERSION}`;
+    });
+  }
 }
 
 
